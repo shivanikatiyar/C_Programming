d5f53f9ea568c3d107b5b0c30b62881deb497dfe
Shivani Katiyar <68283885+shivanikatiyar@users.noreply.github.com>
2020-07-23 10:49:16 +0530

    Menu Restaurant
    Menu Restaurant programm

---
+++ manurestaurants.c
@@ -0,0 +1,95 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int opt1,opt2;
+	float price,Total;
+	printf("\nWelcome to Capsicum\n1 Order the pizza\n2 Order the Noodles\n3 Order the Roll\n:");
+	scanf("%d",&opt1);
+	switch(opt1)
+	{
+		case 1:
+			{
+				printf("\n1cheese burst pizza=149+gst(5)=154.\n2 Indian hot pizza=80+gst(3)=82.\n3 Desi combo pizza=110+gst(4)=114.\n:");
+				scanf("%d",&opt2);
+				switch(opt2)
+				{
+					case 1:
+						{						
+						price=(154*5/100);
+						Total=154-price;
+						printf("\nCheese burst pizza (5 percent discount)total Rs.=%f",Total);
+						break;
+			        	}
+			    	case 2:
+					{
+						price=82*4/100;
+						Total=82-price;
+						printf("\nIndian hot pizza(4 percent discount)total Rs. =%f",Total);
+						break;
+					}
+					case 3:
+						{
+							price=114*5/100;
+							Total=114-price;
+							printf("\n desi combo pizza (5 percent discount)total Rs.=%f",Total);
+							break;
+						}
+			}
+			break;
+	}
+	
+			case 2:
+				{
+					printf("\n1 Veg noodles=75+gst(5)=80.\n2 paneer noodles=95+gst(7)=102.\n:");
+					scanf("%d",&opt2);
+					switch(opt2)
+					{
+						case 1:
+							{
+								price=80*6/100;
+								Total=80-price;
+								printf("\n Veg noodles(6 percent discount)total Rs.=%f",Total);
+								break;
+							}
+							case 2:
+								{
+									price=102*7/100;
+									Total=102-price;
+									printf("\n paneer noodles (7 percent discount)total Rs.=%f",Total);
+									break;
+								}
+					}
+					break;
+				}
+				case 3:
+					{
+						printf("\n1 Veg. roll=45+gst(6)=51.\n2 Cheese roll =65+gst(8)=73.\n:");
+						scanf("%d",&opt2);
+						switch(opt2)
+						{
+							case 1:
+								{
+									price=51*6/100;
+									Total=51-price;
+									printf("\n Veg roll (6 percent discount)total Rs.=%f",Total);
+									break;
+								}
+								case 2:
+									{
+										price=73*7/100;
+										Total=73-price;
+										printf("\nCheese roll (7 percent discount)total Rs.=%f",Total);
+										break;
+									}
+						}
+						break;
+					}
+					default :
+						{
+						printf("\n Wrong choices");}	
+							getch();
+						return 0;
+	}
+	
+}
